4e05fa2618c63d8f39cbefae1bb8959834e20b1b
syedmustafa026 <syedmustafaahmed026@gmail.com>
2021-05-30 22:41:13 +0500

    JS FILE

---
+++ app.js
@@ -0,0 +1,67 @@
+//                               Assignment 35 to 38
+//                                    QUESTION 1      
+
+// function abc(){
+//  var today =   new Date();
+//     console.log(today)
+// }
+// abc()
+
+//                                    QUESTION 2
+
+// function name (){
+//     fname =prompt("Enter first name")
+//     lname =prompt("Enter secont name")
+// alert("HI "+fname +" " +lname)
+// }
+// name()
+
+//                                    QUESTION 3
+
+// var p1 = +prompt ("Enter 1st value")
+// var p2 = +prompt ("Enter 2nd value")
+
+// function abc(a,b){
+//     bc = a + b
+//     alert("The result is " + bc)
+// }
+
+// abc(p1,p2)
+
+//                                    QUESTION 4
+
+
+// var a = (+prompt("1st number"))
+// b = (prompt("any operator"))
+// c = (+prompt("2nd number"))
+// d = ("");
+
+// function cal(){
+//   if(b === "+"){
+//       alert(d = a+c)
+//   }
+//  else if(b === "-"){
+//     alert(d = a-c)}
+
+//    else if(b === "*"){
+//         alert(d = a*c)
+//     }
+//     else if(b === "/"){
+//         alert(d = a/c)
+//     }
+//     alert(d)
+// }
+
+// cal()
+
+//                                    QUESTION 5
+
+
+
+
+
+
+
+
+
+
